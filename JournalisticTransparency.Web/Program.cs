504dd3eb29f26ba77fe7bc50d73fc76c03da39cf
Joppe27 <joppe27@hotmail.com>
2026-02-01 16:20:34 +0100

    Hook up debug menu

--- JournalisticTransparency.Web/Program.cs
+++ JournalisticTransparency.Web/Program.cs
@@ -18,6 +18,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services
     .AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+    .AddOptions()
     .AddBlazorise()
     .AddFluentUI2Providers()
     .AddFluentUIIcons();
