346fa89fdb91900aa7a9107937b17e78c9ed5349
Joppe27 <joppe27@hotmail.com>
2026-03-07 15:06:50 +0100

    Scroll to bottom of elements on opening

--- JournalisticTransparency.Web/Components/Transparency/TransparencyElement.cs
+++ JournalisticTransparency.Web/Components/Transparency/TransparencyElement.cs
@@ -9,6 +9,7 @@ namespace JournalisticTransparency.Web.Components.Transparency;
 public class TransparencyElement : ComponentBase
 {
     [Parameter] public string Style { get; set; } = "";
+    [Parameter] public string ElementId { get; set; } = "";
     [Parameter] public bool Interactive { get; set; }
     [Parameter] public EventCallback DisplayFunctionDisabledToast { get; set; }
     [Parameter] public EventCallback<KeyValuePair<TransparencyElement, bool>> OnOpenStateChanged { get; set; }
---
+++ JournalisticTransparency.Web/wwwroot/interop.js
@@ -0,0 +1,16 @@
+window.enableBeforeUnload = function () {
+    window.onbeforeunload = function (e) {
+        e.preventDefault();
+        return '';
+    };
+};
+
+window.scrollElementIntoView = async function (id) {
+    await delay(300);
+    const element = document.getElementById(id);
+    element.scrollIntoView({ behavior: "smooth", block: "nearest" });
+};
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
