dc84ab2ac286c04a81bdfacb191ae6340fb935b0
Joppe27 <joppe27@hotmail.com>
2026-04-04 16:56:45 +0200

    Also track closing of TrackedAnchors (with asterisks)

--- JournalisticTransparency.Web/wwwroot/interop.js
+++ JournalisticTransparency.Web/wwwroot/interop.js
@@ -11,6 +11,33 @@ window.scrollElementIntoView = async function (id) {
     element.scrollIntoView({behavior: "smooth", block: "nearest"});
 };
 
-function delay(ms) {
+function delay (ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+window.visibility = {
+    handler: null,
+    dotnetComponent: null,
+    
+    addVisibilityRestoredListener: function (dotNet) {
+        this.dotnetComponent = dotNet;
+        
+        this.handler = () => {
+            if (document.visibilityState === "visible") {
+                this.dotnetComponent.invokeMethodAsync("AddClosedInteraction");
+            }
+        };
+        
+        document.addEventListener("visibilitychange", this.handler);
+    },
+
+    removeVisibilityRestoredListener: function ()
+    {
+        if (this.handler) {
+            document.removeEventListener("visibilitychange", this.handler);
+            this.handler = null;
+        }
+        
+        this.dotnetComponent = null;
+    }
 }
