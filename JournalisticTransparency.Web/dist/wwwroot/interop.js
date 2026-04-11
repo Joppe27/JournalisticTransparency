9701573e3954cf5ce5a6c2451578dc914127cd30
Joppe27 <joppe27@hotmail.com>
2026-04-11 13:24:11 +0200

    New release

---
+++ JournalisticTransparency.Web/dist/wwwroot/interop.js
@@ -0,0 +1,49 @@
+window.unload = {
+    enableBeforeUnload: function () {
+        window.onbeforeunload = function (e) {
+            e.preventDefault();
+            return '';
+        };
+    },
+    
+    disableBeforeUnload: function () {
+        window.onbeforeunload = null;
+    },
+}
+
+window.scrollElementIntoView = async function (id) {
+    await delay(300);
+    const element = document.getElementById(id);
+    element.scrollIntoView({behavior: "smooth", block: "nearest"});
+};
+
+function delay (ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+window.visibility = {
+    handler: null,
+    dotnetComponent: null,
+    
+    addVisibilityRestoredListener: function (dotNet) {
+        this.dotnetComponent = dotNet;
+        
+        this.handler = () => {
+            if (document.visibilityState === "visible") {
+                this.dotnetComponent.invokeMethodAsync("AddClosedInteraction");
+            }
+        };
+        
+        document.addEventListener("visibilitychange", this.handler);
+    },
+
+    removeVisibilityRestoredListener: function ()
+    {
+        if (this.handler) {
+            document.removeEventListener("visibilitychange", this.handler);
+            this.handler = null;
+        }
+        
+        this.dotnetComponent = null;
+    }
+}
